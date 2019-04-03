5637994edc9efed530d3fbff5db40e2ca807e586
ceceliastory <ceceliastory@gmail.com>
2019-04-03 14:28:25 -0500

    Include redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
 import * as React from 'react';
-import { render } from 'react-dom';
+import {render} from 'react-dom';
 import App from './components/App';
+import {Provider} from "react-redux";
+import {combineReducers, createStore} from "redux";
+
+const rootReducer = combineReducers({});
+
+const store = createStore(rootReducer);
+const rootElement = document.getElementById('root');
 
 render(
-    <App message="World" />,
-    document.getElementById('root'),
+    <Provider store={store}>
+        <App message={"friend"}/>
+    </Provider>,
+    rootElement
 );
